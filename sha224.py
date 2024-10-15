f88bd3ccf434a94832725b91c866ff5fefc147b1
RH <reganhwang@protonmail.com>
2024-10-15 22:38:06 +0100

    Grp256 up.

--- sha224.py
+++ sha224.py
@@ -2,15 +2,15 @@ from constants import *
 from bitwise_funcs import*
 from preprocessing import*
 
-def sha224(M, form = "hex"):
+def grp256(M, initial_hash, form="hex"):
     '''
     Input: Message string M of any length.
     Output: The sha256 hash for M. If form=="bin" then the output is binary. Otherwise it's hex.
     '''
     M_padded = padding(M)                                    # pad M.
     M_blocks = split_512bit(M_padded)                        # split into 512 bit blocks.
-    H = [0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939, 0xffc00b31, 0x68581511, 0x64f98fa7,0xbefa4fa4]
-                                  # NIST document page 22, section 6.2.2-2:                                                             # intialise hash.
+    H = initial_hash
+                                                             # intialise hash.
     for block in M_blocks:                                   # for each message block...
         W = get_message_schedule(block)                      # obtain message schedule.
         working_variables = update_variables(W,H)            # obtain working variables.
@@ -18,9 +18,20 @@ def sha224(M, form = "hex"):
             H[j] = (H[j]+ working_variables[j])&MASK         # update hash values (section 6.2.2-4.)
     
     if(form=="bin"):
-        return ''.join(format(h, '032b') for h in H)[:224]         # convert final hash values into binary string.
+        return ''.join(format(h, '032b') for h in H)         # convert final hash values into binary string.
 
-    return ''.join(format(h, '08x') for h in H)[:56]              # convert final hash values into hex string.
+    return ''.join(format(h, '08x') for h in H)              # convert final hash values into hex string.
+
+def sha224(M, form = "hex"):
+    '''
+    Input: Message string M of any length.
+    Output: The sha256 hash for M. If form=="bin" then the output is binary. Otherwise it's hex.
+    '''
+    initial_hash = [0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939, 0xffc00b31, 0x68581511, 0x64f98fa7,0xbefa4fa4]
+    if(form=='bin'):
+        return grp256(M, initial_hash, "bin")[:224]
+    else:
+        return grp256(M, initial_hash)[:56]
 
 def update_variables(W,H):
     '''
