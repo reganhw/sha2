975e947cb281d918a5dae372b9df9962f9f97ec1
RH <reganhwang@protonmail.com>
2024-10-16 20:25:50 +0100

    Defined t_lim as a constant.

--- refactor.py
+++ refactor.py
@@ -37,11 +37,11 @@ def get_hash(s0,s1,S0,S1, get_k, M, form="hex",):
 
     def str_to_blocks(s):
         '''
-        Input: String of length 1024n for some n.
+        Input: String of length 512n or 1024n for some n.
         Output: String array, M split into blocks of length bl.
         '''
         if(len(s)&(mbl-1)!=0):
-           raise ValueError("Input string must have length bl*n for some n.")
+           raise ValueError("Input string must have length mbl*n for some n.")
 
         block_num = int(len(s)/mbl)    # number of total blocks
         message_blocks = [s[mbl*i:mbl*(i+1)] for i in range(block_num)]               
@@ -64,20 +64,21 @@ def get_hash(s0,s1,S0,S1, get_k, M, form="hex",):
     M_padded = padding(M)                                  # pad M.
     M_blocks = str_to_blocks(M_padded)                       # split into 1024 bit blocks.
     H = initial_hash_512.copy()
-    def message_schedule(M):
+
+    def message_schedule(s):
         '''
-        Input: String M of length 1024.
-        Output: Array of 80 integers, message schedule as described in the NIST document
+        Input: String M of length 512/1024.
+        Output: List of 64/80 integers, message schedule as described in the NIST document
         '''
-        W = get_word_blocks(M).copy()
-        for t in range(16,80):
+        W = get_word_blocks(s).copy()
+        for t in range(16,t_lim):
             Wt = (s1(W[t-2]) + W[t-7] + s0(W[t-15])+W[t-16])&MASK
             W.append(Wt)
         return W
     
     def my_update_variables(W):
         a,b,c,d,e,f,g,h = H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]
-        for t in range (80):
+        for t in range (t_lim):
             T1 = (h + S1(e) + Ch(e,f,g, MASK) +K512[t] + W[t])&MASK
             T2 = (S0(a) + Maj(a,b,c))&MASK
             h = g
@@ -89,7 +90,7 @@ def get_hash(s0,s1,S0,S1, get_k, M, form="hex",):
             b=a
             a = (T1 + T2)&MASK
         return a,b,c,d,e,f,g,h
-                                                             # intialise hash.
+                                                            
     for block in M_blocks:                                   # for each message block...
         W = message_schedule(block)                          # obtain message schedule.
         working_variables = my_update_variables(W)           # obtain working variables.
